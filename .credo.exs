d2330c4b518125214d5f9265c1d23a1fc44b3477
Paul Fioravanti <paul.fioravanti@gmail.com>
2023-11-20 20:50:17 +1100

    Opt into more credo rules

--- .credo.exs
+++ .credo.exs
@@ -143,16 +143,16 @@
         #
         # Controversial and experimental checks (opt-in, just replace `false` with `[]`)
         #
-        {Credo.Check.Consistency.MultiAliasImportRequireUse, false},
-        {Credo.Check.Design.DuplicatedCode, false},
-        {Credo.Check.Readability.Specs, false},
-        {Credo.Check.Refactor.ABCSize, false},
-        {Credo.Check.Refactor.AppendSingleItem, false},
-        {Credo.Check.Refactor.DoubleBooleanNegation, false},
+        {Credo.Check.Consistency.MultiAliasImportRequireUse, []},
+        {Credo.Check.Design.DuplicatedCode, []},
+        {Credo.Check.Readability.Specs, []},
+        {Credo.Check.Refactor.ABCSize, []},
+        {Credo.Check.Refactor.AppendSingleItem, []},
+        {Credo.Check.Refactor.DoubleBooleanNegation, []},
         {Credo.Check.Refactor.ModuleDependencies, false},
-        {Credo.Check.Refactor.VariableRebinding, false},
-        {Credo.Check.Warning.MapGetUnsafePass, false},
-        {Credo.Check.Warning.UnsafeToAtom, false}
+        {Credo.Check.Refactor.VariableRebinding, []},
+        {Credo.Check.Warning.MapGetUnsafePass, []},
+        {Credo.Check.Warning.UnsafeToAtom, []}
 
         #
         # Custom checks can be created using `mix credo.gen.check`.
--- test/utilities.exs
+++ test/utilities.exs
@@ -1,17 +1,19 @@
 defmodule Utilities do
   # Generates a full error string with an
   # ANSI reset and newline appended to the end.
+  @spec error_string(string()) :: string()
   def error_string(message) do
-    message
-    |> error_string(fn chardata -> IO.ANSI.format(chardata, true) end)
-    |> Kernel.<>("\n")
+    error_string(message, fn chardata -> IO.ANSI.format(chardata, true) end) <>
+      "\n"
   end
 
   # Generates an error string without an
   # ANSI reset or newline appended to the end.
+  @spec error_fragment(string()) :: string()
   def error_fragment(message) do
-    message
-    |> error_string(fn chardata -> IO.ANSI.format_fragment(chardata, true) end)
+    error_string(message, fn chardata ->
+      IO.ANSI.format_fragment(chardata, true)
+    end)
   end
 
   defp error_string(message, ansi_function) do
